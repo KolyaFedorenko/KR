4f9f6b5726e08e3a1c2abf1fefe79fdaf58495e5
usb <usb>
2021-10-02 12:14:04 +0700

    Добавьте файлы проекта.

---
+++ AddWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PRKR
+{
+    /// <summary>
+    /// Логика взаимодействия для AddWindow.xaml
+    /// </summary>
+    public partial class AddWindow : Window
+    {
+        PRKRFEntities context;
+        public AddWindow(PRKRFEntities context, Equipment newEquipment)
+        {
+            InitializeComponent();
+            this.context = context;
+            this.DataContext = newEquipment;
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            context.SaveChanges();
+            this.Close();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PRKR
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PRKRFEntities context;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            context = new PRKRFEntities();
+            ShowTable();
+        }
+        public void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var newEquipment = new Equipment();
+            context.Equipment.Add(newEquipment);
+            var win = new AddWindow(context, newEquipment);
+            win.ShowDialog();
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var row = DataGridRegistrations.SelectedItem as Equipment;
+            if (row==null)
+            {
+                MessageBox.Show("Выберите строку для удаления");
+                return;
+            }
+
+            MessageBoxResult result = MessageBox.Show("Удалить эту строку данных?", "Подтверждение удаления", MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
+            if (result== MessageBoxResult.Yes)
+            {
+                context.Equipment.Remove(row);
+                context.SaveChanges();
+                ShowTable();
+            }
+        }
+
+        public void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void ShowTable()
+        {
+            DataGridRegistrations.ItemsSource = context.Equipment.ToList();
+        }
+    }
+}
